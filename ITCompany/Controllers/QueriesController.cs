5a7181ea5e0c759e11c12e7d4b9bdfcfa10982db
wtwet <yuriygurin1@mail.ru>
2018-05-29 11:38:10 +0300

    test queries via ado.net and ef

--- ITCompany/Controllers/QueriesController.cs
+++ ITCompany/Controllers/QueriesController.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Http;
 using System.Data.SqlClient;
 using System.Data;
 using Microsoft.Extensions.Configuration;
+using System.Threading;
 
 namespace ITCompany.Controllers
 {
@@ -18,7 +19,8 @@ namespace ITCompany.Controllers
     {
         private readonly KvadroContext _context;
         public IConfiguration Configuration { get; }
-
+        string connectionString =
+            "Data Source=DESKTOP-EKQ8IAM\\SQLEXPRESS;Initial Catalog=Kvadro;Integrated Security=True";
         public QueriesController(KvadroContext context)
         {
             _context = context;
@@ -57,8 +59,7 @@ namespace ITCompany.Controllers
             var pageNumber = page ?? 1;
             ViewBag.Type = "ADO.NET";
             List<ProjectInfo> lstproject = new List<ProjectInfo>();
-            string connectionString =
-            "Data Source=DESKTOP-EKQ8IAM\\SQLEXPRESS;Initial Catalog=Kvadro;Integrated Security=True";
+           
             using (SqlConnection con = new SqlConnection(connectionString))
             {
                 SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.ProjectInfo", con);
@@ -75,7 +76,7 @@ namespace ITCompany.Controllers
                     project.Cost = decimal.Parse(rdr["Cost"].ToString());
                     project.ProjectStart = DateTime.Parse(rdr["ProjectStart"].ToString());
                     project.ProjectStop = DateTime.Parse(rdr["ProjectStop"].ToString());
-                   
+
 
                     lstproject.Add(project);
                 }
@@ -101,8 +102,7 @@ namespace ITCompany.Controllers
             var pageNumber = page ?? 1;
             ViewBag.Type = "ADO.NET";
             List<ProjectInfo> lstproject = new List<ProjectInfo>();
-            string connectionString =
-            "Data Source=DESKTOP-EKQ8IAM\\SQLEXPRESS;Initial Catalog=Kvadro;Integrated Security=True";
+           
             using (SqlConnection con = new SqlConnection(connectionString))
             {
                 SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.ProjectInfo", con);
@@ -119,6 +119,164 @@ namespace ITCompany.Controllers
                     project.Cost = decimal.Parse(rdr["Cost"].ToString());
                     project.ProjectStart = DateTime.Parse(rdr["ProjectStart"].ToString());
                     project.ProjectStop = DateTime.Parse(rdr["ProjectStop"].ToString());
+
+
+                    lstproject.Add(project);
+                }
+                con.Close();
+            }
+            var onePage = lstproject.ToPagedList(pageNumber, 5);
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+            ViewBag.OnePage = onePage;
+            ViewBag.time = elapsedTime;
+
+            return View();
+        }
+
+        public IActionResult GetFinishingProjects(int? page)
+        {
+
+            //ViewBag.OnePage = _context.ProjectInfo.Include(e => e.EmployeeInfo).Take(7).ToList().ToPagedList(page ?? 1, 5);
+            var periodFrom = new DateTime(2017, 1, 1, 11, 30, 0);
+            var periodTo = new DateTime(2018, 12, 31, 12, 30, 0);
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var kvadroContext = _context.ProjectInfo.Include(e => e.EmployeeInfo).Where(e => e.ProjectStop.Value > periodFrom && e.ProjectStop.Value < periodTo);
+            var pageNumber = page ?? 1;
+            var onePage = kvadroContext.ToPagedList(pageNumber, 5);
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+            ViewBag.OnePage = onePage;
+            ViewBag.time = elapsedTime;
+            ViewBag.Type = "EF6";
+            return View();
+        }
+
+        public IActionResult GetFinishedProjectsADO(int? page)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var pageNumber = page ?? 1;
+            ViewBag.Type = "ADO.NET";
+            List<ProjectInfo> lstproject = new List<ProjectInfo>();
+           
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.ProjectInfo WHERE ProjectStop BETWEEN '2017-01-01' AND '2018-12-31'; ", con);
+                //cmd.CommandType = CommandType.StoredProcedure;
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    ProjectInfo project = new ProjectInfo();
+                    project.ProjectName = rdr["ProjectName"].ToString();
+                    project.Chief = rdr["Chief"].ToString();
+                    project.Cost = decimal.Parse(rdr["Cost"].ToString());
+                    project.ProjectStart = DateTime.Parse(rdr["ProjectStart"].ToString());
+                    project.ProjectStop = DateTime.Parse(rdr["ProjectStop"].ToString());
+
+
+                    lstproject.Add(project);
+                }
+                con.Close();
+            }
+            var onePage = lstproject.ToPagedList(pageNumber, 5);
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+            ViewBag.OnePage = onePage;
+            ViewBag.time = elapsedTime;
+
+            return View();
+        }
+
+        public IActionResult GetFinishedProjectsPLINQ(int? page)
+        {
+            var periodFrom = new DateTime(2017, 1, 1, 11, 30, 0);
+            var periodTo = new DateTime(2018, 12, 31, 12, 30, 0);
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var pageNumber = page ?? 1;
+            ViewBag.Type = "PLINQ";
+            //string connectionString =
+            //"Data Source=DESKTOP-EKQ8IAM\\SQLEXPRESS;Initial Catalog=Kvadro;Integrated Security=True";
+            //DataContext db = new DataContext(connectionString);
+
+            var lstproject = (from r in _context.ProjectInfo.AsParallel()
+                              where r.ProjectStop > periodFrom
+                              && r.ProjectStop < periodTo
+                              select r);
+            var onePage = lstproject.ToPagedList(pageNumber, 5);
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+            ViewBag.OnePage = onePage;
+            ViewBag.time = elapsedTime;
+
+            return View();
+        }
+
+        public IActionResult GetEmployeesWithProjects(int? page)
+        {
+
+           
+            var periodFrom = new DateTime(2017, 1, 1, 11, 30, 0);
+            var periodTo = new DateTime(2018, 12, 31, 12, 30, 0);
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var kvadroContext = _context.EmployeeInfo.Join(_context.ParticipationInProject, d => d.EmployeeId,
+        f => f.EmployeeId,
+        (d, f) => d);
+            var pageNumber = page ?? 1;
+            var onePage = kvadroContext.ToPagedList(pageNumber, 5);
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+            ViewBag.OnePage = onePage;
+            ViewBag.time = elapsedTime;
+            ViewBag.Type = "EF6";
+            return View();
+        }
+
+        public IActionResult GetEmployeesWithProjectsADO(int? page)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var pageNumber = page ?? 1;
+            ViewBag.Type = "ADO.NET";
+            List<EmployeeInfo> lstproject = new List<EmployeeInfo>();
+            var query = "SELECT * FROM dbo.EmployeeInfo, dbo.ParticipationInProject where dbo.ParticipationInProject.EmployeeId=dbo.EmployeeInfo.EmployeeId";
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, con);
+                //cmd.CommandType = CommandType.StoredProcedure;
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    EmployeeInfo project = new EmployeeInfo();
+                    project.EmployeeName = rdr["EmployeeName"].ToString();
+                    project.Adress = rdr["Adress"].ToString();
+                    project.Year = DateTime.Parse(rdr["Year"].ToString());
+                    project.District = rdr["District"].ToString();
+                    project.Language = rdr["Language"].ToString();
 
 
                     lstproject.Add(project);
